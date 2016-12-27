40fe031af7a3aa15699f570378239f9f9609aae3
joje <joje.attrs@gmail.com>
2016-12-27 16:24:18 +0900

    ..

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: path.join(__dirname, 'x-router-angular.js'),
+  entry: path.join(__dirname, 'lib', 'x-router-angular.js'),
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'x-router-angular.js',
